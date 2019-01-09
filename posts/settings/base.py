5ec8042222d8faa9057a505856e86e1f9e3c569b
Akiva Leffert <aleffert@gmail.com>
2019-01-09 11:17:18 -0500

    Disable http

--- posts/settings/base.py
+++ posts/settings/base.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS: List[Optional[str]] = ['0.0.0.0', 'localhost']
 ALLOWED_HOSTS += [os.getenv('HOST_DOMAIN')] if os.getenv('HOST_DOMAIN') else []
